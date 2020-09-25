85875af5334206ef7e66c547cefafce3d8b5b7f9
thkim9373 <thkim9373@gmail.com>
2020-09-25 20:24:14 +0900

    Save commit.

--- app/src/main/java/com/hoony/applinksassistantexample/MainActivity.kt
+++ app/src/main/java/com/hoony/applinksassistantexample/MainActivity.kt
@@ -2,10 +2,23 @@ package com.hoony.applinksassistantexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hoony.applinksassistantexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val action = intent?.action ?: "action is null"
+        val data = intent?.data
+        val authority = data?.authority
+
+        binding.text.text = "action : $action \n" +
+                "data : ${data?.toString() ?: "data is null"} \n" +
+                "authority : ${authority ?: "authority is null"}"
     }
 }
